8d49a5b2d6a3522547b89df27c62de5dd1901663
Cao Bittencourt <caobittencourt@gmail.com>
2023-09-29 13:00:02 -0300

    atlas.match main v.2.1.0

--- R/fun_match.R
+++ R/fun_match.R
@@ -1,54 +1,54 @@
-# [SETUP] -----------------------------------------------------------------
-# - Packages ----------------------------------------------------------------
-# CRAN packages
-chr_pkg <- c(
-  'bvls', 'fastglm', 'weights' #Regression models
-  , 'devtools' #GitHub packages (temp)
-  , 'readr' #Read data (temp)
-  , 'dplyr', 'tidyr', 'purrr' #Data wrangling
-)
-
-# Git packages
-chr_git <- c(
-  'CaoBittencourt' = 'atlas.kind' #Human capital indispensability coefficient
-)
-
-# Activate / install CRAN packages
-lapply(
-  chr_pkg
-  , function(pkg){
-
-    if(!require(pkg, character.only = T)){
-
-      install.packages(pkg)
-
-    }
-
-    require(pkg, character.only = T)
-
-  }
-)
-
-# Activate / install Git packages
-Map(
-  function(git, profile){
-
-    if(!require(git, character.only = T)){
-
-      install_github(
-        paste0(profile, '/', git)
-        , upgrade = F
-        , force = T
-      )
-
-    }
-
-    require(git, character.only = T)
-
-  }
-  , git = chr_git
-  , profile = names(chr_git)
-)
+# # [SETUP] -----------------------------------------------------------------
+# # - Packages ----------------------------------------------------------------
+# # CRAN packages
+# chr_pkg <- c(
+#   'bvls', 'fastglm', 'weights' #Regression models
+#   , 'devtools' #GitHub packages (temp)
+#   , 'readr' #Read data (temp)
+#   , 'dplyr', 'tidyr', 'purrr' #Data wrangling
+# )
+# 
+# # Git packages
+# chr_git <- c(
+#   'CaoBittencourt' = 'atlas.kind' #Human capital indispensability coefficient
+# )
+# 
+# # Activate / install CRAN packages
+# lapply(
+#   chr_pkg
+#   , function(pkg){
+# 
+#     if(!require(pkg, character.only = T)){
+# 
+#       install.packages(pkg)
+# 
+#     }
+# 
+#     require(pkg, character.only = T)
+# 
+#   }
+# )
+# 
+# # Activate / install Git packages
+# Map(
+#   function(git, profile){
+# 
+#     if(!require(git, character.only = T)){
+# 
+#       install_github(
+#         paste0(profile, '/', git)
+#         , upgrade = F
+#         , force = T
+#       )
+# 
+#     }
+# 
+#     require(git, character.only = T)
+# 
+#   }
+#   , git = chr_git
+#   , profile = names(chr_git)
+# )
 
 # [MATCHING FUNCTIONS] -------------------------------------------------------------
 # - Regression weights --------------------------------------------
